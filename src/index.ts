b00cd9e0f1e3cce6f2f1a4df725a900aab3eb506
Etienne Dldc <e.deladonchamps@gmail.com>
2020-06-23 21:17:46 +0200

    Export KiipOptions

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export * from './types';
-export { Kiip } from './Kiip';
+export { Kiip, KiipOptions } from './Kiip';
 export { MerkleTree } from './MerkleTree';
 export { Timestamp, MutableTimestamp } from './Timestamp';
 export { Clock } from './Clock';
